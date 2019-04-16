0597681e83d8e1515346c7cd1d3bdc677d6501ac
amieeee <1778204448@qq.com>
2019-04-16 20:08:57 +0800

    第四天

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# personal-blog
-个人博客
+# personal-blog 2019.4.12 开始
+
+- 新建项目目录
+  + css/ 存放 css 文件
+  + images/ 存放图片
+  + js/  存放 JavaScript 文件
+  + fonts/ 存放字体图标 阿里巴巴矢量图标 图标在项目 blog 里
+  + index.html 入口文件
+- Atom 快捷键
+  + open-in-browser: ctrl + alt + o
+- 基本知识和问题总结
+  + li:first-child 伪元素选择器 父元素的第一个li元素
+  + background: #000 linear-gradient(to left, #4cd964,#5ac8fa,#007aff,#34aadc,#5856d6,#ff2d55) 渐变色
+  + 字体图标大小设置 font-size
+  + 伪类元素其 ::before content必须有
+  + 子绝父相
+  + .menu:hover span 进入到.menu 元素中 就让 span 选装 180deg
+  `
+  .menu:hover span{
+    transform: rotate(180deg);
+  }
+  `
+  + outline:none; 不显示 轮廓
+  + js文件，有直接操作页面元素的代码，不能在head里面引用 ，而应该放到body内引用代码。
+  + 图标问题 在 .searchico .iconfont 里设置的 margin-top 为什么会影响 .fader .prev, .fader .next 里的margin
+  + 对四个点 添加点击事件和鼠标进入事件 替代 .fader .pager_list li:hover,.fader .pager_list li.active 但是 这个也没有删除 但是点击事件和进入离开事件冲突 不知道怎么解决 已解决：设置一个clickflag = i 具体见代码 这个已经删除了
+  + 轮播图流程
+    1. 获取元素 获取宽度 设置全局变量 pic 记录所在第几张图片
+    2. for 循环为 ol 创建 li 为其添加点击事件 实现 li 颜色切换 和 轮播图片切换
+    3. 克隆 ul 中的第一个 li 放到 ul 最后 === 克隆
+    4. 自动播放 轮播图
+    5. 为 box 添加 鼠标进入和离开事件 为 左右图标添加点击事件
+    6. 点击处理函数 动画移动处理函数
+  + headline 出现了问题 设置浮动没有上去 因为与它的兄弟元素没有设置浮动 所以它才没有浮动上去
---
+++ js/common.js
@@ -0,0 +1,159 @@
+window.onload = function () {
+  // 搜索图标的点击事件
+  // 是否被点击的标志
+  function searchClick () {
+    let flag = true
+    document.getElementById("searchico").onclick = function () {
+      if (flag == true) {
+        document.getElementById("search").style.display =  "block"
+        flag = false
+      } else {
+        document.getElementById("search").style.display =  "none"
+        flag = true
+      }
+    }
+    document.getElementById("close").onclick = function () {
+        document.getElementById("search").style.display = "none"
+    }
+  }
+  searchClick ()
+  function lunbotu () {
+    let box = document.querySelector("#banbox")
+    let screen = document.querySelector(".screen")
+    let imgWidth = screen.offsetWidth
+    let ulObj = screen.children[0]
+    let list = ulObj.children
+    let olObj = screen.children[1]
+    // let arr = screen.children[2]
+    let pic = 0
+    let right = document.querySelector(".next")
+    let left = document.querySelector(".prev")
+    for (let i = 0; i < list.length; i++) {
+      let liObj = document.createElement("li")
+      olObj.appendChild(liObj)
+      liObj.setAttribute("index", i)
+      // 两种实现方式 这是第一种鼠标进入
+      // liObj.onmouseover = function () {
+      //   for (let j = 0; j < olObj.children.length; j++) {
+      //     olObj.children[j].removeAttribute("class")
+      //   }
+      //   this.className = "current"
+      //   pic = this.getAttribute("index")
+      //   animate(ulObj, -pic * imgWidth)
+      // }
+      // 这是第二种鼠标点击 个人认为这种更好一些
+      liObj.onclick = function () {
+        for (let j = 0; j < olObj.children.length; j++) {
+          olObj.children[j].removeAttribute("class")
+        }
+        this.className = "current"
+        pic = this.getAttribute("index")
+        animate(ulObj, -pic * imgWidth)
+      }
+    }
+    olObj.children[0].className = "current"
+    // 克隆一个ul中第一个li,加入到ul中的最后=====克隆
+    ulObj.appendChild(ulObj.children[0].cloneNode(true))
+    // 自动播放
+    let timeId = setInterval(clickHandle, 5000) //与下面的鼠标离开时设置的定时器要统一
+    box.onmouseover = function () {
+      // olObj.style.display = "block"
+      clearInterval(timeId)
+    }
+    box.onmouseout = function () {
+      // olObj.style.display = "none"
+      timeId = setInterval(clickHandle, 5000) // 与上面的统一时间
+    }
+    right.onclick = clickHandle
+    function clickHandle () {
+      if (pic === list.length - 1) {
+        pic = 0
+        ulObj.style.left = 0 + "px"
+      }
+      pic++
+      animate(ulObj, -pic * imgWidth)
+      if (pic === list.length - 1) {
+        olObj.children[olObj.children.length - 1].className = ""
+        olObj.children[0].className = "current"
+      } else {
+        for (let i = 0; i < olObj.children.length; i++) {
+          olObj.children[i].removeAttribute("class")
+        }
+        olObj.children[pic].className = "current"
+      }
+    }
+    left.onclick = function () {
+      if (pic == 0) {
+        pic = 4;
+        ulObj.style.left = -pic * imgWidth + "px";
+      }
+      pic--;
+      animate(ulObj, -pic * imgWidth);
+      //设置小按钮的颜色---所有的小按钮干掉颜色
+      for (var i = 0; i < olObj.children.length; i++) {
+        olObj.children[i].removeAttribute("class");
+      }
+      //当前的pic索引对应的按钮设置颜色
+      olObj.children[pic].className = "current";
+    }
+    function animate (element, target) {
+      clearInterval(element.timeId)
+      element.timeId = setInterval(function () {
+        let current = element.offsetLeft
+        let step = 10
+        step = current < target ? step : -step
+        current += step
+        if (Math.abs(current - target) > Math.abs(step)) {
+          element.style.left = current + "px"
+        } else {
+          clearInterval(element.timeId)
+          element.style.left = target + "px"
+        }
+      }, 1)
+    }
+  }
+  lunbotu()
+
+  // 原来的想法现在被删除了
+  /* // 搜索框的关闭的点击事件
+
+  // 轮播图的序列点的生成 和 点的点击，鼠标进入和离开事件
+  for (let i = 0; i < 4; i++) {
+      let li = document.createElement("li")
+      document.getElementById("pager_list").appendChild(li)
+  }
+  // 点击事件
+  let lis = document.getElementById("pager_list").children
+  // 被点击点的序号标志
+  let clickflag = 0
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].onclick = function () {
+      clickflag = i
+      lis[i].style.background = "#12b7de"
+      for (let j = 0; j < lis.length; j++) {
+        if (i === j) {
+          continue
+        } else {
+          lis[j].style.background = "#fff"
+        }
+      }
+    }
+  }
+  // 鼠标进入事件
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].onmouseover = function () {
+      lis[i].style.opacity = "1"
+      lis[i].style.background = "#12b7de"
+    }
+  }
+  // 鼠标离开事件
+  for (let i = 0 ; i < lis.length; i++) {
+    lis[i].onmouseout = function () {
+      if (i !== clickflag) {
+        lis[i].style.opacity = "1"
+        lis[i].style.background = "#fff"
+      }
+
+    }
+  }*/
+}
